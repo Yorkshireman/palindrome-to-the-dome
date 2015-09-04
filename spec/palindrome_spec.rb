86751794ee9b23589f18af21adb13022d75a2bbd
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-04 14:11:58 +0100

    updated test

--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -5,7 +5,7 @@ describe PalindromeToTheDome do
   context 'when passed a filename' do
 
     it 'counts the number of palindromic lines in the file' do
-      expect(PalindromeToTheDome.call('textfile.md')).to equal(10)
+      expect(PalindromeToTheDome.call('textfile.md')).to equal(99)
     end
 
   end
