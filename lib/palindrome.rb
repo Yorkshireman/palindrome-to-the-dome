2d01fd3a097cc5477de13f27af555069b4778172
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-04 14:08:55 +0100

    solution

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,6 +1,22 @@
 class PalindromeToTheDome
 
   def self.call(filename)
+
+    palindromic_counter = 0
+
+
+    lines_array = []
+    File.readlines(filename).each do |line|
+      lines_array << line
+    end
+
+    lines_array.each do |line|
+      line = line.gsub(/\p{^Alnum}/, '').upcase
+
+      palindromic_counter += 1 if (line == line.reverse)
+    end
+    palindromic_counter
+
   end
 
 end
